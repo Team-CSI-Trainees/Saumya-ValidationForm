1fd77ca68d55e0f1456437108e8a9bcda4f62eb5
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2022-10-09 22:27:30 +0530

    basic js has ben added

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function clearError() {
+  errors = document.getElementsByClassName("formerror");
+  for (let item of error) {
+    item.innerHTML = "";
+  }
+}
 function seterror(id, error) {
   // sets error inside the tag of id
   element = document.getElementById(id);
@@ -7,6 +13,7 @@ function validateform() {
   // it will do the main validation and if validation fails ,set the returnval to false
   var returnval = true;
   var name = document.forms["myForm"]["name"].value;
+
   if (name.length < 2) {
     seterror("name", "*Length of name is too short");
     returnval = false;
@@ -15,15 +22,24 @@ function validateform() {
     seterror("name", "*Length of name cannot be zero");
     returnval = false;
   }
-  var email= document.forms['myForm']["email"].value;
+  var email = document.forms["myForm"]["email"].value;
   if (email.length < 30) {
     seterror("email", "*Length of email is too long");
     returnval = false;
   }
-  if (.length == 0) {
-    seterror("name", "*Length of name cannot be zero");
+  var phone = document.forms["myForm"]["phone"].value;
+  if (phone.length == 10) {
+    seterror("phone", "*Length of phone number should contain 10 digts");
     returnval = false;
   }
-
+  var password = document.forms["myForm"]["password"].value;
+  if (password.length < 10) {
+    seterror("password", "*Length of password cannot contain 10 characters");
+    returnval = false;
+  }
+  var cpassword = document.forms["myForm"]["password"].value;
+  if (cpassword != password) {
+    seterror("cpassword", "password and confirm password shoul match");
+  }
   return returnval;
 }
